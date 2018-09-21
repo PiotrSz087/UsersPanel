313418bfe0ff93e12b48c1983ff44bdf7b2bf54f
Piotr <pszczesny087@gmail.com>
2018-09-21 14:48:25 +0200

    Initial commit

---
+++ src/main/java/com/ps/UsersPanel/UsersPanelApplication.java
@@ -0,0 +1,12 @@
+package com.ps.UsersPanel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsersPanelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsersPanelApplication.class, args);
+	}
+}
